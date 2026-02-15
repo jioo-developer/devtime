2177f1fef3a4257edfc753b3c1caf4f66d8d2d99
김지호 <rlawl3383@gmail.com>
2026-02-15 14:38:46 +0900

    코드 수정

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,22 +1,17 @@
 "use client";
-import React from "react";
-import { useRouter } from "next/navigation";
 import styles from "../asset/404.module.css";
 import CommonButton from "@/atoms/CommonButton/CommonButton";
-const ErrorPage = () => {
-  const router = useRouter();
 
+export default function NotFoundPage() {
   return (
     <div className={styles.notfoundRoot}>
       <div className={styles.notfoundWrapper}>
-        <div className={styles.notfoundBg}></div>
+        <div className={styles.notfoundBg} />
         <h1 className={styles.title}>페이지를 찾을 수 없습니다.</h1>
-        <CommonButton theme="primary" onClick={() => router.back()}>
+        <CommonButton theme="primary" onClick={() => window.history.back()}>
           돌아가기
         </CommonButton>
       </div>
     </div>
   );
-};
-
-export default ErrorPage;
+}
