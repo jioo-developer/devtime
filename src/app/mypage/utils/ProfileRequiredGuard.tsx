d1783edbbdbef6484e5ac02fd71b512483539be9
김지호 <rlawl3383@gmail.com>
2026-07-02 04:59:30 +0900

    코드 수정

--- src/app/mypage/utils/ProfileRequiredGuard.tsx
+++ src/app/mypage/utils/ProfileRequiredGuard.tsx
@@ -1,35 +1,28 @@
 "use client";
-import { useEffect, useRef } from "react";
 import { useRouter } from "next/navigation";
 import { useModalStore } from "@/store/modalStore";
 import CommonButton from "@/components/atoms/CommonButton/CommonButton";
 
 export function ProfileRequiredGuard() {
-  const hasShownModal = useRef(false);
   const router = useRouter();
   const { push: openModal, closeTop: closeModal } = useModalStore();
 
-  useEffect(() => {
-    if (hasShownModal.current) return;
-    hasShownModal.current = true;
-
-    openModal({
-      title: "알림",
-      content: "작성된 프로필이 없습니다. 프로필페이지로 이동하겠습니다.",
-      footer: (
-        <CommonButton
-          theme="primary"
-          onClick={() => {
-            closeModal();
-            router.replace("/profile");
-          }}
-        >
-          확인
-        </CommonButton>
-      ),
-      BackdropMiss: false,
-    });
-  }, [openModal, closeModal, router]);
+  openModal({
+    title: "알림",
+    content: "작성된 프로필이 없습니다. 프로필페이지로 이동하겠습니다.",
+    footer: (
+      <CommonButton
+        theme="primary"
+        onClick={() => {
+          closeModal();
+          router.replace("/profile");
+        }}
+      >
+        확인
+      </CommonButton>
+    ),
+    BackdropMiss: false,
+  });
 
   return null;
 }
